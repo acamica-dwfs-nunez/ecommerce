ba34a1b61d7a0ed56b3f6d2f4790c6c4f482805c
Leonel Vieyra <leonel.vieyra@mercadolibre.com>
2019-06-26 20:33:26 -0300

    started frontend

---
+++ frontend/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import styles from './nav.module.scss'
+
+class Nav extends Component {
+    render() {
+        return <nav className={styles.navbar}>
+            aa
+        </nav>
+    }
+}
+
+export default Nav
---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/Nav'
+
+const Home = () => (
+  <div>
+    <Head title="Home" />
+    <Nav />
+
+  </div>
+)
+
+export default Home
