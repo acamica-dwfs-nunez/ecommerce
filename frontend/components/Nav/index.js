9e698b32eeebfd794d2c17391fc843511d19a2bb
Leonel Vieyra <leonel.vieyra@mercadolibre.com>
2019-06-26 21:47:23 -0300

    added layout

--- frontend/components/Nav/index.js
+++ frontend/components/Nav/index.js
@@ -1,12 +1,32 @@
-import React, {Component} from 'react'
-import styles from './nav.module.scss'
+import React, { Component } from "react";
+import Link from "next/link";
+import styles from "./nav.module.scss";
 
 class Nav extends Component {
-    render() {
-        return <nav className={styles.navbar}>
-            aa
-        </nav>
-    }
+  render() {
+    return (
+      <nav className={styles.navbar}>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <div className={styles.logo} />
+            <input type="text" />
+          </div>
+
+          <div className={styles.right}>
+            <ul>
+              {["Categories", "Hot", "Contact", "About Us"].map(t => (
+                <li>
+                  <Link>
+                    <a href="">{t}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Nav
+export default Nav;
